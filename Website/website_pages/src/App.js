a17da451f0fe462be91acecf8ae7f19866f606bf
AartiWaghmare <Aarti.Waghmare@datarynx.com>
2022-05-06 14:50:16 +0530

    commit

--- Website/website_pages/src/App.js
+++ Website/website_pages/src/App.js
@@ -1,18 +1,17 @@
 import logo from "./logo.svg";
 import './App.css';
-import ConfigurationDropdown from "./Component/ConfigurationDropdown";
-import ClustersDropdown from "./Component/ClustersDropdown";
-import AuditorsDropdown from "./Component/AuditorsDropdown";
-import ReviewideasDropdown from "./Component/ReviewideasDropdown";
+//import ConfigurationDropdown from "./Component/ConfigurationDropdown";
+//import ClustersDropdown from "./Component/ClustersDropdown";
+//import AuditorsDropdown from "./Component/AuditorsDropdown";
+//import ReviewideasDropdown from "./Component/ReviewideasDropdown";
+import ReviewideasApi from "./Component/ReviewideasApi";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <ConfigurationDropdown/>
-    <ClustersDropdown/>
-    <AuditorsDropdown/>
-    <ReviewideasDropdown/>
+    
+    <ReviewideasApi/>
 
   </div>
 
